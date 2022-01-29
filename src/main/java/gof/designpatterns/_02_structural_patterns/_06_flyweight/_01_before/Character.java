ec262e5318499535f878fa03962668eb755cd8e2
IcedAmeri <seokhyunkho@gmail.com>
2022-01-30 00:57:15 +0900

    [구조 관련 디자인 패턴] 플라이웨이트 패턴 - 패턴 소개

---
+++ src/main/java/gof/designpatterns/_02_structural_patterns/_06_flyweight/_01_before/Character.java
@@ -0,0 +1,18 @@
+package gof.designpatterns._02_structural_patterns._06_flyweight._01_before;
+
+public class Character {
+    private char value;
+
+    private String color;
+
+    private String fontFamily;
+
+    private int fontSize;
+
+    public Character(char value, String color, String fontFamily, int fontSize) {
+        this.value = value;
+        this.color = color;
+        this.fontFamily = fontFamily;
+        this.fontSize = fontSize;
+    }
+}
