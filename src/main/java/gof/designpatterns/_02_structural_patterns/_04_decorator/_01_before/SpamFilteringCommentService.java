bf67bd4437f83598f7398d7ec5dbeacb80507d5b
IcedAmeri <seokhyunkho@gmail.com>
2022-01-29 23:57:38 +0900

    [객체 생성 관련 디자인 패턴] 데코레이터 패턴 - 패턴 소개

---
+++ src/main/java/gof/designpatterns/_02_structural_patterns/_04_decorator/_01_before/SpamFilteringCommentService.java
@@ -0,0 +1,17 @@
+package gof.designpatterns._02_structural_patterns._04_decorator._01_before;
+
+public class SpamFilteringCommentService extends CommentService {
+
+    @Override
+    public void addComment(String comment) {
+        boolean isSpam = isSpam(comment);
+        if (!isSpam) {
+            super.addComment(comment);
+        }
+    }
+
+    private boolean isSpam(String comment) {
+        return comment.contains("http");
+    }
+
+}
