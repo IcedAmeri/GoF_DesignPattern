f8be10a4b85259cb2b81e703ef8273b9bc56cbc7
IcedAmeri <seokhyunkho@gmail.com>
2022-02-05 00:43:53 +0900

    [행동 관련 디자인 패턴] 인터프리터 패턴

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_03_interpreter/_02_after/PostfixExpression.java
@@ -0,0 +1,9 @@
+package gof.designpatterns._03_behavioral_patterns._03_interpreter._02_after;
+
+import java.util.Map;
+
+public interface PostfixExpression {
+
+    int interpret(Map<Character, Integer> context);
+
+}
