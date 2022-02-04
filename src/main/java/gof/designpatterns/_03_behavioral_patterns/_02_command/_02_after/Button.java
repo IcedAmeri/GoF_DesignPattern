316228faa853dc4306045b5e94d57fa8bf32bb1d
IcedAmeri <seokhyunkho@gmail.com>
2022-02-04 22:32:55 +0900

    [행동 관련 디자인 패턴] 커맨드 패턴 - 자바와 스프링에서 찾아보는 패턴

--- src/main/java/gof/designpatterns/_03_behavioral_patterns/_02_command/_02_after/Button.java
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_02_command/_02_after/Button.java
@@ -1,21 +1,30 @@
 package gof.designpatterns._03_behavioral_patterns._02_command._02_after;
 
+import gof.designpatterns._03_behavioral_patterns._02_command._01_before.Game;
 import gof.designpatterns._03_behavioral_patterns._02_command._01_before.Light;
 
-public class Button {
-    private Command command;
+import java.util.Stack;
 
-    public Button(Command command) {
-        this.command = command;
+public class Button {
+    private Stack<Command> commands = new Stack<>();
+
+    public void press(Command command) {
+        command.execute();
+        commands.push(command);
     }
 
-    public void press() {
-        command.execute();
+    public void undo() {
+        if (!commands.isEmpty()) {
+            Command command = commands.pop();
+            command.undo();
+        }
     }
 
     public static void main(String[] args) {
-        Button button = new Button(new LightOnCommand(new Light()));
-        button.press();
-        button.press();
+        Button button = new Button();
+        button.press(new GameStartCommand(new Game()));
+        button.press(new LightOnCommand(new Light()));
+        button.undo();
+        button.undo();
     }
 }
---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_02_command/_03_java/CommandInJava.java
@@ -0,0 +1,21 @@
+package gof.designpatterns._03_behavioral_patterns._02_command._03_java;
+
+import gof.designpatterns._03_behavioral_patterns._02_command._01_before.Game;
+import gof.designpatterns._03_behavioral_patterns._02_command._01_before.Light;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CommandInJava {
+
+    public static void main(String[] args) {
+        Light light = new Light();
+        Game game = new Game();
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.submit(light::on);
+        executorService.submit(game::start);
+        executorService.submit(game::end);
+        executorService.submit(light::off);
+        executorService.shutdown();
+    }
+}
