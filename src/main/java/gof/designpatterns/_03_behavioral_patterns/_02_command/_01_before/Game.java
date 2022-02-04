5475987a626550f9ccc6ced5012cbd309923967c
IcedAmeri <seokhyunkho@gmail.com>
2022-02-04 22:20:04 +0900

    [행동 관련 디자인 패턴] 커맨드 패턴 - 패턴 소개

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_02_command/_01_before/Game.java
@@ -0,0 +1,20 @@
+package gof.designpatterns._03_behavioral_patterns._02_command._01_before;
+
+public class Game {
+
+    private boolean isStarted;
+
+    public void start() {
+        System.out.println("게임을 시작합니다.");
+        this.isStarted = true;
+    }
+
+    public void end() {
+        System.out.println("게임을 종료합니다.");
+        this.isStarted = false;
+    }
+
+    public boolean isStarted() {
+        return isStarted;
+    }
+}
