715cee28e3cb4684345f9e0c2b00babe381b9617
IcedAmeri <seokhyunkho@gmail.com>
2022-02-06 00:02:26 +0900

    [행동 관련 디자인 패턴] 중재자 패턴 - 패턴 소개

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_05_mediator/_01_before/Gym.java
@@ -0,0 +1,11 @@
+package gof.designpatterns._03_behavioral_patterns._05_mediator._01_before;
+
+public class Gym {
+
+    private CleaningService cleaningService;
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+
+}
---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_05_mediator/_01_before/Restaurant.java
@@ -0,0 +1,12 @@
+package gof.designpatterns._03_behavioral_patterns._05_mediator._01_before;
+
+public class Restaurant {
+    private CleaningService cleaningService = new CleaningService();
+    public void dinner(Guest guest) {
+        System.out.println("dinner " + guest);
+    }
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
