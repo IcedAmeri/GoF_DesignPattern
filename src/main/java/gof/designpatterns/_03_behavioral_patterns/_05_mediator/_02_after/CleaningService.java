d32c623feb18a45985f64caec6edbd54fbf6ff88
IcedAmeri <seokhyunkho@gmail.com>
2022-02-06 00:08:09 +0900

    [행동 관련 디자인 패턴] 중재자 패턴 - 패턴 적용하기

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_05_mediator/_02_after/CleaningService.java
@@ -0,0 +1,11 @@
+package gof.designpatterns._03_behavioral_patterns._05_mediator._02_after;
+
+public class CleaningService {
+
+    private FrontDesk frontDesk = new FrontDesk();
+
+    public void getTowers(Integer guestId, int numberOfTowers) {
+        String roomNumber = this.frontDesk.getRoomNumberFor(guestId);
+        System.out.println("provide " + numberOfTowers + " to " + roomNumber);
+    }
+}
---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_05_mediator/_02_after/Restaurant.java
@@ -0,0 +1,9 @@
+package gof.designpatterns._03_behavioral_patterns._05_mediator._02_after;
+
+import java.time.LocalDateTime;
+
+public class Restaurant {
+    public void dinner(Integer id, LocalDateTime dateTime) {
+
+    }
+}
