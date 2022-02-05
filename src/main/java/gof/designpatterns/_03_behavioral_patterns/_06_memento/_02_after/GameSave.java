789e723b62d23fcfec0ea7e6baec4eb37deef39b
IcedAmeri <seokhyunkho@gmail.com>
2022-02-06 00:50:02 +0900

    [행동 관련 디자인 패턴] 메멘토 패턴 - 패턴 적용하기

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_06_memento/_02_after/GameSave.java
@@ -0,0 +1,20 @@
+package gof.designpatterns._03_behavioral_patterns._06_memento._02_after;
+
+public class GameSave {
+    private final int blueTeamScore;
+
+    private final int redTeamScore;
+
+    public GameSave(int blueTeamScore, int redTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+}
