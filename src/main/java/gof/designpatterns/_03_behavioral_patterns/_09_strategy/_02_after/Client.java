a7fe7a3a271ca27dcd305178b6e977981808c61c
IcedAmeri <seokhyunkho@gmail.com>
2022-02-06 09:08:19 +0900

    [행동 관련 디자인 패턴] 전략 패턴 - 패턴 소개 및 적용

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_09_strategy/_02_after/Client.java
@@ -0,0 +1,21 @@
+package gof.designpatterns._03_behavioral_patterns._09_strategy._02_after;
+
+public class Client {
+    public static void main(String[] args) {
+        BlueLightRedLight game = new BlueLightRedLight();
+        game.blueLight(new Normal());
+        game.redLight(new Faster());
+        game.blueLight(new Speed() {
+            @Override
+            public void blueLight() {
+                System.out.println("blue light");
+            }
+
+            @Override
+            public void redLight() {
+                System.out.println("red light");
+            }
+        });
+
+    }
+}
---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_09_strategy/_02_after/Speed.java
@@ -0,0 +1,6 @@
+package gof.designpatterns._03_behavioral_patterns._09_strategy._02_after;
+
+public interface Speed {
+    void blueLight();
+    void redLight();
+}
