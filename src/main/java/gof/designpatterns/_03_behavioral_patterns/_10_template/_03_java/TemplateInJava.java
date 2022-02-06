76779ad23dae1222e701434f416dc1feff9f39e4
IcedAmeri <seokhyunkho@gmail.com>
2022-02-06 09:44:35 +0900

    [행동 관련 디자인 패턴] 템플릿 메소드 패턴 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/gof/designpatterns/_03_behavioral_patterns/_10_template/_03_java/TemplateInJava.java
@@ -0,0 +1,10 @@
+package gof.designpatterns._03_behavioral_patterns._10_template._03_java;
+
+import javax.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+    public static void main(String[] args) {
+        HttpServlet httpServlet;
+    }
+}
